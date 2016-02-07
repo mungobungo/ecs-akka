eb068e9aadc5b1f7203f860dfe25c28797e665f4
Oleg Taykalo <oleg.taykalo@gmail.com>
2016-02-07 01:21:14 +0100

    initial nancy server done

---
+++ akka-nancy/HelloModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+namespace akkanansy
+{
+	public class HelloModule : NancyModule
+	{
+		public HelloModule()
+		{
+			Get["/"] = x => {
+
+				return string.Format("{0} {1}", DateTime.UtcNow, "waiting for the miracle");
+			};
+		}
+	}
+}
+
---
+++ akka-nancy/Program.cs
@@ -0,0 +1,21 @@
+﻿using Nancy.Hosting.Self;
+using System;
+namespace akkanansy
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			var nancyHost = new NancyHost(new Uri("http://localhost:8888/nancy/"), new Uri("http://127.0.0.1:8889/nancy/"), new Uri("http://localhost:8899/nancytoo/"));
+			nancyHost.Start();
+
+			Console.WriteLine("Nancy now listening - navigating to http://localhost:8888/nancy/. Press enter to stop");
+			//Process.Start("http://localhost:8888/nancy/");
+			Console.ReadKey();
+
+			nancyHost.Stop();
+
+			Console.WriteLine("Stopped. Good bye!");
+		}
+	}
+}
